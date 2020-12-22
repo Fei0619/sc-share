99cc56454830ed7b0f15bf637d2a0291b7cc7b9c
LX_feishicheng <feiwork@189.cn>
2020-12-22 16:07:30 +0800

    时间处理工具类

---
+++ sc-share-common/src/main/java/date/DateFormatter.kt
@@ -0,0 +1,22 @@
+package date
+
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * @author 费世程
+ * @date 2020/12/22 15:01
+ */
+object DateFormatter {
+
+  private val map = ConcurrentHashMap<String, DateTimeFormatter>()
+
+  fun of(pattern: String = DatePattern.yyyy_MM_dd_HH_mm_ss,
+         locale: Locale = Locale.SIMPLIFIED_CHINESE)
+      : DateTimeFormatter {
+    val key = "$pattern:${locale.language}:${locale.country}"
+    return map.computeIfAbsent(key) { DateTimeFormatter.ofPattern(pattern, locale) }
+  }
+
+}
---
+++ sc-share-common/src/main/java/date/DateUtils.java
@@ -0,0 +1,37 @@
+package date;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 费世程
+ * @date 2020/12/22 14:31
+ */
+public class DateUtils {
+
+  //Java7中的SimpleDateFormat不是线程安全的，可以用ThreadLocal来解决这个问题
+  private static final Map<String, ThreadLocal<SimpleDateFormat>> SDF_MAP = new HashMap<>();
+
+  /**
+   * 根据模式串获取一个SimpleDateFormat实例
+   *
+   * @param pattern 模式串
+   * @return SimpleDateFormat实例
+   */
+  public static SimpleDateFormat getSimpleDateFormat(String pattern) {
+    ThreadLocal<SimpleDateFormat> threadLocal = SDF_MAP.get(pattern);
+    if (threadLocal == null) {
+      synchronized (pattern) {
+        threadLocal = SDF_MAP.get(pattern);
+        if (threadLocal == null) {
+          threadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat(pattern));
+          SDF_MAP.put(pattern, threadLocal);
+        }
+      }
+    }
+    return threadLocal.get();
+  }
+
+
+}
